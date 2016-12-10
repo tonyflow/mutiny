332a6c6fe2ea5127c9ebd7885590c58136c034a5
Pinelopi Kouleri <pinelopi.kouleri@upstreamsystems.com>
2016-12-10 16:07:02 +0200

    feed page

--- feed.js
+++ feed.js
@@ -1,25 +1,6 @@
-angular.module('feedModule', ['ngResource'])
-    .factory('FeedLoader', function ($resource) {
-        return $resource('/post/list', {}, {
-            fetch: { method: 'JSONP', params: {categories: 'music'} }
-        });
-    })
-    .service('FeedList', function ($rootScope, FeedLoader) {
-        this.get = function() {
-            if (feeds.length === 0) {
-                for (var i=0; i<feedSources.length; i++) {
-                    FeedLoader.fetch({}, function (data) {
-                        var feed = data.responseData.feed;
-                        feeds.push(feed);
-                    });
-                }
-            }
-            return feeds;
-        };
-    })
-    .controller('FeedCtrl', function ($scope, FeedList) {
-        $scope.feeds = FeedList.get();
-        $scope.$on('FeedList', function (event, data) {
-            $scope.feeds = data;
+angular.module('mutiny', [])
+    .controller('PostCtrl', function ($scope, $http) {
+        $http.get('http://10.16.4.153:8080/post/list').then(function (response) {
+            $scope.posts = response.data;
         });
     });
